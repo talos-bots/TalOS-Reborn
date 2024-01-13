7d49d8eee8d2ca5592ae0772d334024277a6aeaa
thesentinel2615 <nevyn202@outlook.com>
2024-01-12 18:30:51 -0600

    frontend room routes

---
+++ src/api/roomAPI.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { Room } from '../types';
+
+const apiClient = axios.create({
+    baseURL: '/api/rooms'
+});
+
+async function getRooms() {
+    return await apiClient.get('/').then(response => response.data).catch(err => console.error(err));
+}
+
+async function getRoomById(id: string) {
+    return await apiClient.get(`/${id}`).then(response => response.data).catch(err => console.error(err));
+}
+
+async function saveRoom(roomData: Room) {
+    return await apiClient.post('/save', roomData).then(response => response.data).catch(err => console.error(err));
+}
+
+async function removeRoomById(id: string) {
+    return await apiClient.delete(`/${id}`).then(response => response.data).catch(err => console.error(err));
+}
+
+export { getRooms, getRoomById, saveRoom, removeRoomById };
