697985634f7d7b5f4579760ca351cf061deac51d
thesentinel2615 <nevyn202@outlook.com>
2024-01-14 00:56:06 -0600

    Update roomPipeline.ts

--- server/services/discordBot/roomPipeline.ts
+++ server/services/discordBot/roomPipeline.ts
@@ -119,7 +119,7 @@ export class RoomPipeline implements Room {
         this.characters = [];
         this.updateLastModified();
     }
-    
+
     addAlias(alias: Alias): void {
         this.aliases.push(alias);
         this.updateLastModified();
