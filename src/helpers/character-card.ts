2be70f9bd3d14cffababfd2da8c1b0f20ba8481a
thesentinel2615 <nevyn202@outlook.com>
2024-04-05 15:14:10 -0500

    update card parser

--- src/helpers/character-card.ts
+++ src/helpers/character-card.ts
@@ -27,202 +27,213 @@ export const useWindowSize = () => {
 
 export const importTavernCharacter = (file: File): Promise<Character> => {
     return new Promise((resolve, reject) => {
-        console.log("Parsing Tavern Card");
-
-        exifr.parse(file).then(async (metadata) => {
-            console.log("Tavern Card Parsed");
-            if (metadata) {
-                const isCardV2 = metadata.chara;
-                if (isCardV2) {
-                    console.log("New Tavern Card Detected");
-                    const decodedString = atob(metadata.chara);
-                    const cardSpec = JSON.parse(decodedString);
-                    let characterData;
-                    if (cardSpec?.data !== undefined) {
-                        characterData = cardSpec.data;
-                    } else {
-                        console.log("Old Tavern Card Detected");
-                        characterData = cardSpec;
-                    }
-                    console.log(characterData);
-                    const construct = await processCharacterData(characterData, );
-                    resolve(construct);
-                } else {
-                    tryParseOldCard(file).then((construct) => {
-                        if (construct) {
-                            resolve(construct);
-                        } else {
-                            reject(new Error("Failed to parse old card"));
-                        }
-                    });
-                }
-            }
-        }).catch((error) => {
-            console.log("Tavern Card Parse Failed", error);
-            tryParseOldCard(file).then((construct) => {
-                console.log("Exif parser failure. Trying old parser");
+      console.log("Parsing Tavern Card");
+  
+      exifr
+        .parse(file)
+        .then(async (metadata) => {
+          console.log("Tavern Card Parsed");
+          if (metadata) {
+            const isCardV2 = metadata.chara;
+            if (isCardV2) {
+              console.log("New Tavern Card Detected");
+              const decodedString = atob(metadata.chara);
+              const cardSpec = JSON.parse(decodedString);
+              let characterData;
+              if (cardSpec?.data !== undefined) {
+                characterData = cardSpec.data;
+              } else {
+                console.log("Old Tavern Card Detected");
+                characterData = cardSpec;
+              }
+              console.log(characterData);
+              const construct = await processCharacterData(characterData);
+              resolve(construct);
+            } else {
+              tryParseOldCard(file).then((construct) => {
                 if (construct) {
-                    resolve(construct);
+                  resolve(construct);
                 } else {
-                    reject(new Error("Failed to parse old card"));
+                  reject(new Error("Failed to parse old card"));
                 }
-            }).catch((error) => {
-                console.log("Old Tavern Card Parse Failed", error);
-                reject(error);
+              });
+            }
+          }
+        })
+        .catch((error) => {
+          console.log("Tavern Card Parse Failed", error);
+          tryParseOldCard(file)
+            .then((construct) => {
+              console.log("Exif parser failure. Trying old parser");
+              if (construct) {
+                resolve(construct);
+              } else {
+                reject(new Error("Failed to parse old card"));
+              }
+            })
+            .catch((error) => {
+              console.log("Old Tavern Card Parse Failed", error);
+              reject(error);
             });
         });
     });
-};
-
-
-async function processCharacterData(characterData: any): Promise<Character> {
+  };
+  
+  async function processCharacterData(characterData: any): Promise<Character> {
     console.log(characterData);
-    if(characterData?.data !== undefined){
-        characterData = characterData.data;
+    if (characterData?.data !== undefined) {
+      characterData = characterData.data;
     }
     const construct = new Character();
-    construct.name = (characterData.name || '').replaceAll('\r', '');
-    construct.scenario = (characterData.scenario || '').replaceAll('\r', '');
-
-    if(characterData.description){
-        construct.description = characterData.description.replaceAll('\r', '');
+    construct.name = (characterData.name || "").replaceAll("\r", "");
+    construct.scenario = (characterData.scenario || "").replaceAll("\r", "");
+  
+    // Construct personality field by filtering out empty or null parts
+    if (characterData.description) {
+      construct.description = characterData.description.replaceAll("\r", "");
     }
-    
+  
     if (characterData.personality) {
-        construct.personality = characterData.personality.trim().replaceAll('\r', '')
+      construct.personality = characterData.personality
+        .trim()
+        .replaceAll("\r", "");
     }
-
+  
     if (characterData.creator_notes) {
-        construct.creator_notes = characterData.creator_notes.replaceAll('\r', '');
+      construct.creator_notes = characterData.creator_notes.replaceAll("\r", "");
     }
-
+  
     if (characterData.mes_example) {
-        construct.mes_example = characterData.mes_example.replaceAll('\r', '');
+      construct.mes_example = characterData.mes_example.replaceAll("\r", "");
     }
-
+  
     if (characterData.post_history_instructions) {
-        construct.post_history_instructions = characterData.post_history_instructions.replaceAll('\r', '');
+      construct.post_history_instructions =
+        characterData.post_history_instructions.replaceAll("\r", "");
     }
-
-    if (characterData.creator) {
-        construct.creator = characterData.creator.replaceAll('\r', '');
-    }
-
-    if (characterData.avatar) {
-        construct.avatar = characterData.avatar;
-    }
-
-    if (characterData.tags) {
-        construct.tags = characterData.tags;
-    }
-
-    if(characterData.mes_example){
-        construct.mes_example = characterData.mes_example.replaceAll('\r', '');
-    }
-
+  
     if (characterData.first_mes && characterData.first_mes.trim().length > 0) {
-        construct.setFirstMes(characterData.first_mes.replaceAll('\r', ''));
+      construct.setFirstMes(characterData.first_mes.replaceAll("\r", ""));
     }
-
-    if(characterData.system_prompt && characterData.system_prompt.trim().length > 0){
-        construct.system_prompt = characterData.system_prompt.replaceAll('\r', '')
+  
+    if (
+      characterData.alternate_greetings &&
+      characterData.alternate_greetings.length > 0 &&
+      Array.isArray(characterData.alternate_greetings)
+    ) {
+      characterData.alternate_greetings.forEach((greeting: string) => {
+        construct.alternate_greetings.push(greeting.replaceAll("\r", ""));
+      });
     }
-    if (characterData.alternate_greetings && characterData.alternate_greetings.length > 0 && Array.isArray(characterData.alternate_greetings)) {
-        characterData.alternate_greetings.forEach((greeting: string) => {
-            construct.alternate_greetings.push(greeting.replaceAll('\r', ''));
-        });
+  
+    if (
+      characterData.thought_pattern &&
+      characterData.thought_pattern.trim().length > 0
+    ) {
+      construct.thought_pattern = characterData.thought_pattern.replaceAll(
+        "\r",
+        ""
+      );
     }
-    if(characterData.thought_pattern && characterData.thought_pattern.trim().length > 0){
-        construct.thought_pattern = characterData.thought_pattern.replaceAll('\r', '')
+  
+    if (
+      characterData.visual_description &&
+      characterData.visual_description.trim().length > 0
+    ) {
+      construct.visual_description = characterData.visual_description.replaceAll(
+        "\r",
+        ""
+      );
     }
-
-    if(characterData.visual_description && characterData.visual_description.trim().length > 0){
-        construct.visual_description = characterData.visual_description.replaceAll('\r', '')
-    }
-
-    if(characterData.system_prompt && characterData.system_prompt.trim().length > 0){
-        construct.system_prompt = characterData.system_prompt.replaceAll('\r', '')
+  
+    if (
+      characterData.system_prompt &&
+      characterData.system_prompt.trim().length > 0
+    ) {
+      construct.system_prompt = characterData.system_prompt.replaceAll("\r", "");
     }
     return construct;
-}
-interface Chunk {
+  }
+  interface Chunk {
     name: string;
     data: any; // Replace 'any' with a more specific type if possible
-}
-
-const tryParseOldCard = (file: File): Promise<Character | void> => {
+  }
+  
+  const tryParseOldCard = (file: File): Promise<Character | void> => {
     console.log("Old Tavern Card Detected");
     return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        
-        reader.onload = async (event) => {
-            if (event.target && event.target.result instanceof ArrayBuffer) {
-                const arrayBuffer = event.target.result;
-                const uint8Array = new Uint8Array(arrayBuffer);
-                const chunks: Chunk[] = extract(uint8Array);
-                
-                const textChunks = chunks.filter((chunk: Chunk) => chunk.name === 'tEXt').map((chunk: Chunk) => {
-                    return PNGtext.decode(chunk.data);
-                });
-                
-                if (textChunks.length > 0) {
-                    try {
-                        const decodedString = decodeBase64(textChunks[0].text);
-                        const _json = JSON.parse(decodedString);
-                        const construct = await processCharacterData(_json);
-                        resolve(construct);
-                    } catch (error) {
-                        console.error('Error parsing JSON:', error);
-                        reject(error);
-                    }
-                } else {
-                    reject();
-                }
+      const reader = new FileReader();
+  
+      reader.onload = async (event) => {
+        if (event.target && event.target.result instanceof ArrayBuffer) {
+          const arrayBuffer = event.target.result;
+          const uint8Array = new Uint8Array(arrayBuffer);
+          const chunks: Chunk[] = extract(uint8Array);
+  
+          const textChunks = chunks
+            .filter((chunk: Chunk) => chunk.name === "tEXt")
+            .map((chunk: Chunk) => {
+              return PNGtext.decode(chunk.data);
+            });
+  
+          if (textChunks.length > 0) {
+            const decodedString = decodeBase64(textChunks[0].text);
+            try {
+              const _json = JSON.parse(decodedString);
+              const construct = await processCharacterData(_json);
+              resolve(construct);
+            } catch (error) {
+              console.error("Error parsing JSON:", error);
+              reject(error);
             }
-        };
-      
-        reader.onerror = (error) => {
-            console.error('Error reading file:', error);
-            reject(error);
-        };
-      
-        reader.readAsArrayBuffer(file);
+          } else {
+            reject();
+          }
+        }
+      };
+  
+      reader.onerror = (error) => {
+        console.error("Error reading file:", error);
+        reject(error);
+      };
+  
+      reader.readAsArrayBuffer(file);
     });
-};
-
-const decodeBase64 = (base64String: string) => {
+  };
+  
+  const decodeBase64 = (base64String: string) => {
     const text = atob(base64String);
     return decodeURIComponent(escape(text));
-};
-
-export async function createTavernCardV2(construct: Character): Promise<any> {
+  };
+  
+  export async function createTavernCardV2(construct: Character): Promise<any> {
     const data = {
-        name: construct.name || "",
-        description: "",
-        personality: construct.personality || "",
-        scenario: construct.scenario || "",
-        first_mes: construct.first_mes || "",
-        thought_pattern: construct.thought_pattern || "",
-        visual_description: construct.visual_description || "",
-        mes_example: construct.mes_example || "",
-        creator_notes: construct.creator_notes || "",
-        system_prompt: construct.system_prompt || "",
-        post_history_instructions: construct.post_history_instructions || "",
-        alternate_greetings: construct.alternate_greetings || [],
-        character_book: undefined,
-        tags: construct.tags || [],
-        creator: construct.creator || "",
-        character_version: "v2",
-        extensions: {},
+      name: construct.name || "",
+      description: construct.description || "",
+      personality: construct.personality || "",
+      scenario: construct.scenario || "",
+      first_mes: construct.first_mes || "",
+      thought_pattern: construct.thought_pattern || "",
+      visual_description: construct.visual_description || "",
+      mes_example: construct.mes_example || "",
+      creator_notes: construct.creator_notes || "",
+      system_prompt: construct.system_prompt || "",
+      post_history_instructions: construct.post_history_instructions || "",
+      alternate_greetings: construct.alternate_greetings || [],
+      character_book: undefined,
+      tags: construct.tags || [],
+      creator: "",
+      character_version: "v2",
+      extensions: {},
     };
-
+  
     return {
-        spec: 'chara_card_v2',
-        spec_version: '2.0',
-        ...data,
-        data,
+      spec: "chara_card_v2",
+      spec_version: "2.0",
+      ...data,
+      data,
     };
-}
+  }
 
 export async function saveTavernCardAsImage(construct: Character) {
     const tavernCardV2 = await createTavernCardV2(construct);
